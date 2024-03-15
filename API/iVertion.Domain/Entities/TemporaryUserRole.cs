52fde351f1c622e9fbe06b62c94b4d9cd2854c16
eduardorspada <dudryspada@gmail.com>
2024-03-14 21:03:56 -0300

    We created the private method that validates the domain properties.

--- API/iVertion.Domain/Entities/TemporaryUserRole.cs
+++ API/iVertion.Domain/Entities/TemporaryUserRole.cs
@@ -1,4 +1,6 @@
 
+using iVertion.Domain.Validation;
+
 namespace iVertion.Domain.Entities
 {
     public sealed class TemporaryUserRole : Entity
@@ -6,5 +8,26 @@ namespace iVertion.Domain.Entities
         public string? Role { get; private set; }
         public string? TargetUserId { get; private set; }
         public DateTime? ExpirationDate { get; private set; }
+
+        private void ValidationDomain(string role,
+                                      string targetUserId,
+                                      DateTime? expirationDate)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(role),
+                                           "Invalid Role, must not be null or empty.");
+            DomainExceptionValidation.When(role.Length < 5,
+                                           "Invalid Role, too short, must be at least 5 characters long.");
+            DomainExceptionValidation.When(role.Length > 25,
+                                           "Invalid Role, too long, max 25 characters.");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(targetUserId),
+                                           "Invalid Target User Id, must not be null or empty.");
+            DomainExceptionValidation.When(expirationDate > DateTime.Now,
+                                           "Invalid Expiration Date, must be greater than the current date.");
+
+            Role = role;
+            TargetUserId = targetUserId;
+            ExpirationDate = expirationDate;
+            
+        }
     }
 }
